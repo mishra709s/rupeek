5803274cfc00b55a7c1e24c418349ca74a9fde2a
soubhagya-addvector <soubhagya@addvector.com>
2020-03-09 11:49:23 +0530

    Utility for launching browser added

--- src/main/java/atm/repeek/utils/Utility.java
+++ src/main/java/atm/repeek/utils/Utility.java
@@ -1,6 +1,41 @@
 package atm.repeek.utils;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Utility 
 {
+	protected WebDriver driver = null;
 
+	public void launchBrowser() {
+		try {
+
+			// Set the driver path
+			System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+			
+			// Create instance of the driver
+			driver = new ChromeDriver();
+			
+			// Implicit wait for elements
+			driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+			
+			// Delete all cookies
+			driver.manage().deleteAllCookies();
+			
+			// Maximize the browser
+			driver.manage().window().maximize();
+			
+			// Wait for page load
+			Thread.sleep(3000);
+			
+			// URL Launching
+			driver.get("http://automationpractice.com/index.php");
+			Thread.sleep(3000);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
